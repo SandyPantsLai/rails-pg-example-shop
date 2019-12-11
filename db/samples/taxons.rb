7b5a0993bda196bce0e14a557d78e832825dd5c4
Sandy Lai <sandy.lai@sandylai-ltm.internal.salesforce.com>
2019-12-11 17:50:40 -0500

    Correct taxons seedings to handle blank parent field in dataframe

--- db/samples/taxons.rb
+++ db/samples/taxons.rb
@@ -3,21 +3,27 @@ require "daru"
 ["parts", "themes"].each do |taxonomy_name|
   taxonomy = Spree::Taxonomy.where(name: I18n.t("spree.taxonomies.#{taxonomy_name}")).first_or_create!
   parent_taxon = Spree::Taxon.where(name: I18n.t("spree.taxonomies.#{taxonomy_name}")).first_or_create!
-  df = Daru::DataFrame.from_csv(File.join(__dir__, "#{taxonomy_name}-taxons.csv"))
+
+  df = Daru::DataFrame.from_csv(File.join(__dir__, "initial-#{taxonomy_name}-taxons.csv"))
 
   # Find all unique parent taxons and create them, all nested under the main parent taxon
   df.uniq("parent")["parent"].each do |parent|
-    taxon = parent_taxon.children.where(name: parent).first_or_create!
-    taxon.permalink = taxon.permalink.gsub("parts/", "")
-    taxon.taxonomy = taxonomy
-    taxon.save!
+    unless parent.nil?
+      taxon = parent_taxon.children.where(name: parent, position: 0).first_or_create!
+      taxon.permalink = taxon.permalink.gsub("taxonomy_name/", "")
+      taxon.taxonomy = taxonomy
+      taxon.save!
+    end
   end
 
   # create children taxons to nest under each parent taxon
   df.each(:row) do |row|
-    parent = Spree::Taxon.where(name: row["parent"]).first
+    row["parent"].nil? ? parent_name = I18n.t("spree.taxonomies.#{taxonomy_name}") : parent_name = row["parent"] 
+    puts parent_name
+    parent = Spree::Taxon.where(name: parent_name).first
+    
     taxon = parent.children.where(name: row["name"]).first_or_create!
-    taxon.permalink = taxon.permalink.gsub("parts/#{row['parent']}", "")
+    taxon.permalink = taxon.permalink.gsub("taxonomy_name/#{row['parent']}", "")
     taxon.taxonomy = taxonomy
     taxon.save!
   end
@@ -26,7 +32,7 @@ end
 category_taxonomy = Spree::Taxonomy.where(name: I18n.t("spree.taxonomies.category")).first_or_create!
 category_parent_taxon = Spree::Taxon.where(name: I18n.t("spree.taxonomies.category")).first_or_create!
 ["Bestsellers", "Sale"].each do |taxon_name|
-  taxon =  category_parent_taxon.children.where(name: taxon_name).first_or_create!
+  taxon =  category_parent_taxon.children.where(name: taxon_name, position: 0).first_or_create!
   taxon.permalink = taxon.permalink.gsub("categories/", "")
   taxon.taxonomy = category_taxonomy
   taxon.save!
--- lib/store_sample.rb
+++ lib/store_sample.rb
@@ -10,13 +10,13 @@ module StoreSample
       Store::Sample.load_sample('taxons')
       Store::Sample.load_sample('stock_locations')
       Store::Sample.load_sample('properties')
-      Store::Sample.load_sample('products')
-      Spree::Sample.load_sample('addresses')
-      Spree::Sample.load_sample('payment_methods')
       Spree::Sample.load_sample('shipping_categories')
-      Spree::Sample.load_sample('shipping_methods')
       Spree::Sample.load_sample('tax_categories')
       Spree::Sample.load_sample('tax_rates')
+      Store::Sample.load_sample('products')
+      Spree::Sample.load_sample('addresses')
+      Spree::Sample.load_sample('shipping_methods')
+      Spree::Sample.load_sample('payment_methods')
     end
   end
 end
