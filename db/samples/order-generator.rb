f31eb60865c47a6fd12da2e0073855e8df209c9f
Sandy Lai <sandy.lai@sandylai-ltm.internal.salesforce.com>
2020-01-15 13:24:48 -0500

    Update to make order generation factor by always using new address instead of retrieving

--- db/samples/order-generator.rb
+++ db/samples/order-generator.rb
@@ -1,7 +1,16 @@
+Faker::Config.locale = 'en'
 
-Store::Sample.load_sample('address-generator', true)
-
-address = Spree::Address.all.sample
+address = Spree::Address.create!(
+  firstname: Faker::Name.first_name,
+  lastname: Faker::Name.last_name,
+  address1: Faker::Address.street_address,
+  address2: Faker::Address.secondary_address,
+  city: Faker::Address.city,
+  state: Spree::State.find_by!(name: "New York"),
+  zipcode: Faker::Address.zip_code(state_abbreviation: "NY"),
+  country: Spree::Country.find_by!(iso3: 'USA'),
+  phone: Faker::PhoneNumber.phone_number
+)
 
 all_products = Spree::Product.all
 
