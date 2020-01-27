68b12689b0d863c71d6364665508cd8c17751c69
Sandy Lai <sandy.lai@sandylai-ltm.internal.salesforce.com>
2020-01-27 13:51:03 -0500

    adjust Bundler.require

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,8 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+# Bundler.require(*Rails.groups)
+Bundler.require(Rails.env, :default)
 
 module RailsPgExampleShop
   class Application < Rails::Application
@@ -48,5 +49,3 @@ module RailsPgExampleShop
     # the framework and any gems in your application.
   end
 end
-
-require 'appoptics_apm'
