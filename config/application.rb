db811a0bba16c7b17f61bd28f4b3b27ac21136c2
Sandy Lai <sandy.lai@sandylai-ltm.internal.salesforce.com>
2020-01-28 11:49:11 -0500

    Fix group name in application.rb

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 # Bundler.require(*Rails.groups)
-Bundler.require(:default, Rails.env, :appoptics)
+Bundler.require(:default, Rails.env, :appoptics_apm)
 
 module RailsPgExampleShop
   class Application < Rails::Application
