8d01e81d4d6d736810141d5ddab78de2ff7f667c
Sandy Lai <sandy.lai@sandylai-ltm.internal.salesforce.com>
2020-03-03 13:48:12 -0500

    Authorize rack-mini-profiler if user is admin, no extra params needed.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,7 @@ class ApplicationController < ActionController::Base
 	before_action :check_rack_mini_profiler 
 	def check_rack_mini_profiler
 	  if try_spree_current_user.try(:has_spree_role?, "admin")
-		if params[:rmp] 
-			Rack::MiniProfiler.authorize_request
-		end
+		Rack::MiniProfiler.authorize_request
 	  end
 	end
 end
