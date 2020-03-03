5f57a57a470ad678487ede7050f3a4b66ca5df68
Sandy Lai <sandy.lai@sandylai-ltm.internal.salesforce.com>
2020-03-03 15:02:21 -0500

    Add initializer for rack-mini-profiler and update README

---
+++ config/initializers/rack_mini_profiler.rb
@@ -0,0 +1,12 @@
+# set MemoryStore
+Rack::MiniProfiler.config.storage = Rack::MiniProfiler::MemoryStore
+
+# set RedisStore
+if Rails.env.production?
+  uri = URI.parse(ENV["REDIS_SERVER_URL"])
+  Rack::MiniProfiler.config.storage_options = { :host => uri.host, :port => uri.port, :password => uri.password }
+  Rack::MiniProfiler.config.storage = Rack::MiniProfiler::RedisStore
+end
+
+# enable advanced tools to see GC profiling
+Rack::MiniProfiler.config.enable_advanced_debugging_tools = true
