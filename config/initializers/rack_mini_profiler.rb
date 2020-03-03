5925347b518b0c595838eb40638811170a3a96aa
Sandy Lai <sandy.lai@sandylai-ltm.internal.salesforce.com>
2020-03-03 15:29:50 -0500

    Correct initializer to use proper config var.

--- config/initializers/rack_mini_profiler.rb
+++ config/initializers/rack_mini_profiler.rb
@@ -3,7 +3,7 @@ Rack::MiniProfiler.config.storage = Rack::MiniProfiler::MemoryStore
 
 # set RedisStore
 if Rails.env.production?
-  uri = URI.parse(ENV["REDIS_SERVER_URL"])
+  uri = URI.parse(ENV["REDIS_URL"])
   Rack::MiniProfiler.config.storage_options = { :host => uri.host, :port => uri.port, :password => uri.password }
   Rack::MiniProfiler.config.storage = Rack::MiniProfiler::RedisStore
 end
